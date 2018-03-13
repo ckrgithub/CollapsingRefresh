88ba2b0c9cd3c4cf439b94bb4532089d6feacfb4
ckrgithub <13560177212@163.com>
2018-03-13 22:23:06 +0800

    1.优化代码

--- app/src/main/java/com/ckr/collapsingrefresh/view/OneFragment.java
+++ app/src/main/java/com/ckr/collapsingrefresh/view/OneFragment.java
@@ -30,145 +30,149 @@ import static com.ckr.smartrefresh.util.LogUtil.Logd;
  */
 
 public class OneFragment extends BaseFragment implements OnRefreshLoadmoreListener, AppBarLayout.OnOffsetChangedListener, OnOffsetListener {
-    private static final String TAG = "OneFragment";
-    @BindView(R.id.recyclerView)
-    SmoothRecyclerView recyclerView;
-    @BindView(R.id.refreshLayout)
-    SmartRefreshLayout smartRefreshLayout;
-    @BindDimen(R.dimen.size_5)
-    int paddingSize;
-    private MyAdapter mAdapter;
-    static OnSmartListener scrollListener;
-    private boolean isVisible;
-    private static Handler handler = new Handler(Looper.myLooper());
-    private int verticalOffset;
+	private static final String TAG = "OneFragment";
+	@BindView(R.id.recyclerView)
+	SmoothRecyclerView recyclerView;
+	@BindView(R.id.refreshLayout)
+	SmartRefreshLayout smartRefreshLayout;
+	@BindDimen(R.dimen.size_5)
+	int paddingSize;
+	private MyAdapter mAdapter;
+	static OnSmartListener scrollListener;
+	private boolean isVisible;
+	private Handler handler = new Handler(Looper.myLooper());
+	private int verticalOffset;
 
 
-    public static OneFragment newInstance(OnSmartListener onScrollListener) {
-        scrollListener = onScrollListener;
-        Bundle args = new Bundle();
-        OneFragment fragment = new OneFragment();
-        fragment.setArguments(args);
-        return fragment;
-    }
+	public static OneFragment newInstance(OnSmartListener onScrollListener) {
+		scrollListener = onScrollListener;
+		Bundle args = new Bundle();
+		OneFragment fragment = new OneFragment();
+		fragment.setArguments(args);
+		return fragment;
+	}
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (handler != null) {
-            handler = null;
-        }
-        if (scrollListener != null) {
-            scrollListener.removeOnOffsetChangedListener(this);
-        }
-    }
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if (handler != null) {
+			handler = null;
+		}
+		if (scrollListener != null) {
+			scrollListener.removeOnOffsetChangedListener(this);
+		}
+	}
 
-    @Override
-    protected int getContentLayoutId() {
-        return R.layout.fragment_base;
-    }
+	@Override
+	protected int getContentLayoutId() {
+		return R.layout.fragment_base;
+	}
 
-    @Override
-    protected void init() {
-        Logd(TAG, "init: target:"+recyclerView);
-        scrollListener.addOnOffsetChangedListener(this);
-        recyclerView.setOnSmoothScrollListener(scrollListener);
-        smartRefreshLayout.setOnRefreshLoadmoreListener(this);
-        smartRefreshLayout.setOnOffsetListener(this);
-        setAdapter();
-    }
+	@Override
+	protected void init() {
+		Logd(TAG, "init: target:" + recyclerView);
+		scrollListener.addOnOffsetChangedListener(this);
+		recyclerView.setOnSmoothScrollListener(scrollListener);
+		smartRefreshLayout.setOnRefreshLoadmoreListener(this);
+		smartRefreshLayout.setOnOffsetListener(this);
+		setAdapter();
+	}
 
-    protected void setAdapter() {
-        mAdapter = new MyAdapter(getContext());
-        LinearLayoutManager layout = new LinearLayoutManager(getContext());
-        layout.setSmoothScrollbarEnabled(true);
-        layout.setAutoMeasureEnabled(true);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setLayoutManager(layout);
-        recyclerView.setAdapter(mAdapter);
-        initData();
-    }
+	protected void setAdapter() {
+		mAdapter = new MyAdapter(getContext());
+		LinearLayoutManager layout = new LinearLayoutManager(getContext());
+		layout.setSmoothScrollbarEnabled(true);
+		layout.setAutoMeasureEnabled(true);
+		recyclerView.setItemAnimator(new DefaultItemAnimator());
+		recyclerView.setLayoutManager(layout);
+		recyclerView.setAdapter(mAdapter);
+		initData();
+	}
 
-    private void initData() {
-        List<AlbumList> datas = new ArrayList<>();
-        AlbumList albumList = new AlbumList();
-        albumList.setTitle("风继续吹");
-        try {
-            for (int i = 0; i < 12; i++) {
-                AlbumList clone = (AlbumList) albumList.clone();
-                clone.setUserName("item  " + i);
-                if (i % 2 == 0) {
-                    clone.setDrawableId(R.mipmap.banner);
-                } else {
-                    clone.setDrawableId(R.mipmap.banner2);
-                }
-                datas.add(clone);
-            }
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
-        mAdapter.updateAll(datas);
-    }
+	private void initData() {
+		List<AlbumList> datas = new ArrayList<>();
+		AlbumList albumList = new AlbumList();
+		albumList.setTitle("风继续吹");
+		try {
+			for (int i = 0; i < 12; i++) {
+				AlbumList clone = (AlbumList) albumList.clone();
+				clone.setUserName("item  " + i);
+				if (i % 2 == 0) {
+					clone.setDrawableId(R.mipmap.banner);
+				} else {
+					clone.setDrawableId(R.mipmap.banner2);
+				}
+				datas.add(clone);
+			}
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		mAdapter.updateAll(datas);
+	}
 
-    @Override
-    protected void onVisible() {
-        isVisible = true;
-    }
+	@Override
+	protected void onVisible() {
+		isVisible = true;
+	}
 
-    @Override
-    protected void onInvisible() {
-        isVisible=false;
-    }
+	@Override
+	protected void onInvisible() {
+		isVisible = false;
+	}
 
-    @Override
-    public void refreshFragment() {
-        if (isVisible) {
-            scrollListener.setScrollTarget(recyclerView);
-            recyclerView.setCurrentScrollY();
-        }
-    }
+	@Override
+	public void refreshFragment() {
+		if (isVisible) {
+			scrollListener.setScrollTarget(recyclerView);
+			recyclerView.setCurrentScrollY();
+		}
+	}
 
-    @Override
-    public void onRefresh(RefreshLayout refreshlayout) {
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                smartRefreshLayout.finishRefresh();
-            }
-        }, 2000);
-    }
+	@Override
+	public void onRefresh(RefreshLayout refreshlayout) {
+		if (handler != null) {
+			handler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					smartRefreshLayout.finishRefresh();
+				}
+			}, 2000);
+		}
+	}
 
-    @Override
-    public void onLoadmore(RefreshLayout refreshlayout) {
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                smartRefreshLayout.finishLoadmore();
-            }
-        }, 2000);
-    }
+	@Override
+	public void onLoadmore(RefreshLayout refreshlayout) {
+		if (handler != null) {
+			handler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					smartRefreshLayout.finishLoadmore();
+				}
+			}, 2000);
+		}
+	}
 
-    @Override
-    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-        this.verticalOffset = verticalOffset;
-        Logd(TAG, "onOffsetChanged: verticalOffset:" + this.verticalOffset);
-        if (verticalOffset != 0) {
-            boolean enableRefresh = smartRefreshLayout.isEnableRefresh();
-            if (enableRefresh) {
-                smartRefreshLayout.setEnableRefresh(false);
-            }
-        } else {
-            smartRefreshLayout.setEnableRefresh(true);
-        }
-    }
+	@Override
+	public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+		this.verticalOffset = verticalOffset;
+		Logd(TAG, "onOffsetChanged: verticalOffset:" + this.verticalOffset);
+		if (verticalOffset != 0) {
+			boolean enableRefresh = smartRefreshLayout.isEnableRefresh();
+			if (enableRefresh) {
+				smartRefreshLayout.setEnableRefresh(false);
+			}
+		} else {
+			smartRefreshLayout.setEnableRefresh(true);
+		}
+	}
 
-    @Override
-    public int getTotalRange() {
-        return scrollListener==null?0:scrollListener.getTotalRange();
-    }
+	@Override
+	public int getTotalRange() {
+		return scrollListener == null ? 0 : scrollListener.getTotalRange();
+	}
 
-    @Override
-    public int getCurrentOffset() {
-        return verticalOffset;
-    }
+	@Override
+	public int getCurrentOffset() {
+		return verticalOffset;
+	}
 }
