26b8aa89d5c25e1e2757278c793a7cc91154e70a
ckrgithub <13560177212@163.com>
2018-04-08 22:18:18 +0800

    1.使用notifyItemChanged(int)更新adapter

--- app/src/main/java/com/ckr/collapsingrefresh/adapter/BaseAdpater.java
+++ app/src/main/java/com/ckr/collapsingrefresh/adapter/BaseAdpater.java
@@ -14,75 +14,76 @@ import java.util.List;
  * Created by PC大佬 on 2018/2/9.
  */
 
-public abstract class BaseAdpater<T,E extends BaseViewHolder> extends RecyclerView.Adapter<E> {
-    private static final String TAG = "BaseAdpater";
-    protected Context mContext;
-    protected List<T> data;
+public abstract class BaseAdpater<T, E extends BaseViewHolder> extends RecyclerView.Adapter<E> {
+	private static final String TAG = "BaseAdpater";
+	protected Context mContext;
+	protected List<T> data;
 
-    public BaseAdpater(Context context) {
-        mContext = context;
-        data = new ArrayList<>();
-    }
+	public BaseAdpater(Context context) {
+		mContext = context;
+		data = new ArrayList<>();
+	}
 
-    public BaseAdpater(Context context, List<T> data) {
-        mContext = context;
-        this.data = data;
-    }
+	public BaseAdpater(Context context, List<T> data) {
+		mContext = context;
+		this.data = data;
+	}
 
-    public List<T> getData() {
-        return data;
-    }
+	public List<T> getData() {
+		return data;
+	}
 
-    public void updateAll(List newData) {
-        if (newData == null || newData.size() == 0) {
-            Log.d(TAG, "updateAll: newData == null || newData.size() == 0");
-            return;
-        }
-        data.clear();
-        data.addAll(newData);
-        notifyDataSetChanged();
-    }
+	public void updateAll(List newData) {
+		if (newData == null || newData.size() == 0) {
+			Log.d(TAG, "updateAll: newData == null || newData.size() == 0");
+			return;
+		}
+		data.clear();
+		data.addAll(newData);
+		notifyDataSetChanged();
+	}
 
-    public void updateItem(int start, T t) {
-        if (t == null) {
-            return;
-        }
-        if (start < 0 && start > data.size()) {
-            throw new ArrayIndexOutOfBoundsException(start);
-        }
-        data.add(start, t);
-        int len = data.size() - start;
-        notifyItemRangeChanged(start, len);
-    }
+	public void updateItem(int start, T t) {
+		if (t == null) {
+			return;
+		}
+		if (start < 0 && start > data.size()) {
+			throw new ArrayIndexOutOfBoundsException(start);
+		}
+		data.add(start, t);
+		int len = data.size() - start;
+		notifyItemChanged(start);
+//		notifyDataSetChanged();
+	}
 
-    public void updateItem(T t) {
-        if (t == null) {
-            return;
-        }
-        int len = data.size();
-        data.add(t);
-        notifyItemRangeChanged(len, 1);
-    }
+	public void updateItem(T t) {
+		if (t == null) {
+			return;
+		}
+		int len = data.size();
+		data.add(t);
+		notifyItemRangeChanged(len, 1);
+	}
 
 
-    @Override
-    public E onCreateViewHolder(ViewGroup parent, int viewType) {
-        return getViewHolder(LayoutInflater.from(mContext).inflate(getLayoutId(viewType), parent, false), viewType);
-    }
+	@Override
+	public E onCreateViewHolder(ViewGroup parent, int viewType) {
+		return getViewHolder(LayoutInflater.from(mContext).inflate(getLayoutId(viewType), parent, false), viewType);
+	}
 
-    @Override
-    public void onBindViewHolder(E holder, int position) {
-        convert(holder, position, data.get(position));
-    }
+	@Override
+	public void onBindViewHolder(E holder, int position) {
+		convert(holder, position, data.get(position));
+	}
 
-    @Override
-    public int getItemCount() {
-        return data.size();
-    }
+	@Override
+	public int getItemCount() {
+		return data.size();
+	}
 
-    protected abstract E getViewHolder(View itemView, int viewType);
+	protected abstract E getViewHolder(View itemView, int viewType);
 
-    protected abstract void convert(E holder, int position, T t);
+	protected abstract void convert(E holder, int position, T t);
 
-    protected abstract int getLayoutId(int viewType);
+	protected abstract int getLayoutId(int viewType);
 }
